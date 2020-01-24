35f03e408447cf83711366f1db8bac6a8147f914
topotyn <46344900+topotyn@users.noreply.github.com>
2020-01-24 18:12:17 +0200

    Update epel.pp
    add comments

--- epel.pp
+++ epel.pp
@@ -1,5 +1,5 @@
-class web_server::epel{
- package { 'epel-release':
+class web_server::epel{ # создание класса в нутри модуля
+ package { 'epel-release': # указываю что мне нужно установить пакет epel-release
   ensure => installed,
   }
 }
