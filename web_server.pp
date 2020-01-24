477ea24d24c52fd099fb7673b2df7f2165da343a
topotyn <46344900+topotyn@users.noreply.github.com>
2020-01-24 18:16:13 +0200

    Update web_server.pp
    add comments

--- web_server.pp
+++ web_server.pp
@@ -1,8 +1,8 @@
-node 'client1.pup.com' {
- include web_server::epel
- include web_server::nginx
+node 'client1.pup.com' { # создал ноду, имя ноды совпадает и именем сертификата/hostname клиента
+ include web_server::epel # подгружаю модуль
+ include web_server::nginx # подгружаю модуль
 }
 
-node 'client2.pup.com' {
- include web_server::apache
+node 'client2.pup.com' { # создал ноду для второго клиента
+ include web_server::apache # подгружаю модуль
 }
